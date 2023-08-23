6da79bbb3be04dd0e8c74d4bf9b8ea3dca4bba3a
Julio Donado <julio22567@hotmail.com>
2023-08-23 14:32:35 -0500

    Rol validation was added in the user creation endpoint

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -3,6 +3,7 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 const { usuariosGet, usuariosPost, usuariosPut, usuariosDelete, usuariosPatch } = require('../controllers/usuarios.controller');
 const { validarCampos } = require('../middlewares/validar-campos');
+const Rol = require('../models/role');
 
 const router = Router();
 
@@ -12,7 +13,15 @@ router.post('/', [
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'El password debe ser de mas de 6 letras').isLength({min: 6}),
     check('correo', 'El correo no es valido').isEmail(),
-    check('rol', 'No es un rol valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    // check('rol', 'No es un rol valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('rol').custom(async (rol = '') => {
+        const existeRol = await Rol.findOne({ rol })
+
+        if (!existeRol) {
+            throw new Error(`El rol ${rol} no esta registrado en la BD`)
+        }
+
+    }), 
     validarCampos
 ], usuariosPost)
 
